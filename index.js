fb843b7e2271a6fcd0a9575a56ea40c21c9922ea
Tife <boluwatife1904@gmail.com>
2022-10-22 12:52:24 +0100

    animate on scroll addition

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-gsap.from(".img-contain", {
-  duration: 2,
-  y: -300,
-  opacity: 0,
-  stagger: 0.5,
-  ease: "back",
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+const observer2 = new IntersectionObserver((entries2) => {
+  entries2.forEach((entry2) => {
+    if (entry2.isIntersecting) {
+      gsap.from(".img-contain", {
+        duration: 2,
+        y: -300,
+        opacity: 0,
+        stagger: 0.5,
+        ease: "back",
+      });
+    }
+  });
 });
 
-gsap.from(".gallery", {
-  opacity: 0,
-  y: 200,
-  duration: 2,
-});
+const pageSections = document.querySelectorAll(".section");
+const gallery = document.querySelector(".gallery");
+pageSections.forEach((section) => observer.observe(section));
+observer2.observe(gallery);
