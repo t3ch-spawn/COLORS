0b8d0947054ea25a6efb264229e97480763ba827
Tife <boluwatife1904@gmail.com>
2022-10-26 01:17:08 +0100

    created mobile nav. added animations to mobile nav

--- index.js
+++ index.js
@@ -21,10 +21,34 @@ const gallery = document.querySelector(".gallery");
 pageSections.forEach((section) => observer.observe(section));
 observer2.observe(gallery);
 
-// gsap.from(".img-contain", {
-//   duration: 1.5,
-//   y: -300,
-//   opacity: 0,
-//   stagger: 0.5,
-//   ease: "back",
-// });
+const hamburger = document.querySelector(".navigation__hamburger");
+const navRight = document.querySelector(".navigation__right");
+const overlay = document.querySelector(".overlay");
+let has = false;
+
+hamburger.addEventListener("click", () => {
+  navRight.classList.toggle("active");
+  overlay.classList.toggle("active");
+
+  if (navRight.classList.contains("active")) {
+    gsap
+      .timeline()
+
+      .from(".navigation__item-container", {
+        duration: 1,
+        y: 40,
+        opacity: 0,
+      })
+      .from(".navigation__cta", {
+        x: 70,
+        opacity: 0,
+        duration: 0.7,
+        ease: "back",
+      });
+  }
+});
+
+overlay.addEventListener("click", () => {
+  navRight.classList.remove("active");
+  overlay.classList.remove("active");
+});
